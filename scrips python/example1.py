20a152c391f447760471e43e470c2fd04dcdecf8
andres hidalgo <andrew.ahc22@gmail.com>
2024-08-23 21:01:23 -0500

    Scrip python 1

---
+++ scrips python/example1.py
@@ -0,0 +1,14 @@
+#Este es un comentario de linea
+''' Este es un comentario
+de bloque
+'''
+data = ''' esto es una variable string en bloqu
+'''
+n1= 10
+n2 = 20
+suma = n1+n2
+print(suma)
+
+print ("La suma es:",suma)
+print (f"La suma es: {suma}")
+print ("La suma es",n1+n2)
