92fab7c20a365e9c84fdb3b643b2f5e3f9349cf3
halier rojas <halierandres26@gmail.com>
2024-09-13 21:35:29 -0500

    actualizacion videojuego

---
+++ scrips_python/number_race/main.py
@@ -0,0 +1,12 @@
+'''
+Scripts description numbre race
+dev: halier rojas
+date 13/08/2024
+'''
+
+from random import randint
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
+print(f"Dice 1:{dice1}")
+print(f"Dice 2:{dice2}")
